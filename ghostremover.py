49c86781d505ae5e0a53b7cf5555588ed42767e8
Esther Hutcherson <paulhutcherson1030@gmail.com>
2021-03-31 16:40:21 -0400

    Update ghostremover.py

--- ghostremover.py
+++ ghostremover.py
@@ -2,10 +2,12 @@ import pydivert
 import threading
 import requests
 import json
+import socket
 
 blockedips = set()
 playerips = set()
-requestips = set()
+
+playerips.add(socket.gethostbyname(socket.gethostname()))
 
 def block(ip):
 	if not type(ip) is str:
@@ -23,12 +25,22 @@ def block(ip):
 	except:
 		print("Failed to write to C:/eu4_blocked_ips.log. (unnecessary for script to function)")
 
+def unblock(ip):
+	if not type(ip) is str:
+		print("IP must be in string format. An example block would be: unblock('69.168.1.30')")
+		return
+
+	blockedips.remove(ip)
+
+	print("Successfully removed " + ip + " from the block list.")
+	
+
 def loadlogfile():
 	try:
 		f2 = open("C:/eu4_blocked_ips.log", "r")
 		ips = f2.read().split("\n")
 		for x in ips:
-			print(x)
+			print("Blocking " + x)
 			blockedips.add(x)
 		f2.close()
 	except:
@@ -41,7 +53,7 @@ def getinfo(ip):
 	try:
 		r = requests.get("http://ipinfo.io/" + ip + "/json", verify=False)
 	except:
-		print("Connection refused by ipinfo for " + ip + ", check if this is a Valve server manually.")
+		print("\nConnection refused by ipinfo for " + ip + ", check if this is a Valve server manually.")
 		return	
 	
 	data = r.json()
@@ -50,27 +62,25 @@ def getinfo(ip):
 		blockedips.add(ip)
 		print("Blocked NAT attmept from " + ip)
 	else:
-		print("New connection from " + ip + " from " + data["region"] + ", " + data["country"] + ".")
+		print("\nNew connection from " + ip + " from " + data["region"] + ", " + data["country"] + ".")
 
 def networking():
 	with pydivert.WinDivert("udp.PayloadLength > 0") as w:
 		for packet in w:
 			if "6e2801004b28" in packet.payload.hex() and packet.src_addr not in playerips:
-				if not packet.src_addr in requestips:
+				if not packet.src_addr in playerips:
 					playerips.add(packet.src_addr)
-					requestips.add(packet.src_addr)
 					try:			
 						getinfo(packet.src_addr)
 					except:
-						print("New connection from " + packet.src_addr)
+						print("\nNew connection from " + packet.src_addr)
 			if packet.src_addr in blockedips or packet.dst_addr in blockedips:
 				packet.payload = "\x00\x01\x02".encode()
 			w.send(packet)
 
-def shitass(): # required multithreading to allow user input without pausing windivert
-	print("\nEU4 Ghost Remover by Mastoid & KLS loaded.\n")
-	print("Type loadlogfile() to load previously blocked IP addresses into the current session. (recommended)")
-	print("Type block('ip here') to block an IP address.")
+threading.Thread(name='networking', target=networking).start() # required multithreading to allow user input without pausing windivert
 
-threading.Thread(name='networking', target=networking).start()
-threading.Thread(name='shitass', target=shitass).start()
+print("\nEU4 Ghost Remover by Mastoid & KLS loaded.\n")
+print("Type loadlogfile() to load previously blocked IP addresses into the current session. (recommended)")
+print("Type block('ip here') to block an IP address.")
+print("Type unblock('ip here') to unblock an IP address.")
